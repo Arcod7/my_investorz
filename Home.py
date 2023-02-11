2bc2f8d19296fbc9ce270d60a6d43cb38adec2d6
sbruschet <samuel.bruschet@gmail.com>
2023-02-11 13:32:45 +0100

    [ADD] multi-pages

---
+++ Home.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+import csv
+import pandas as pd
+import streamlit as st
---
+++ pages/Data.py
@@ -0,0 +1,16 @@
+import csv
+import pandas as pd
+import streamlit as st
+
+st.title("MY_INVESTORZ")
+st.markdown("# Data 🎈")
+df = pd.read_csv('./data/train.csv')
+df = df.set_index("PassengerId")
+st.write("And here's the data for view:")
+sentence = st.text_input('Type if you want to research any information in the database:')
+# if sentence == df[0]:
+    # st.write(my_model.predict(sentence))
+# else:
+st.write(df)
+st.sidebar.markdown("# Data 🎈")
+
