42c8af102504bc7f06c9dc6e3dcdd6b4b3eff1e8
Matthias von Rakowski <matthias.von-rakowski@epitech.eu>
2023-02-11 13:48:34 +0100

    [FIX] fix good data for the page data

--- pages/Data.py
+++ pages/Data.py
@@ -4,8 +4,8 @@ import streamlit as st
 
 st.title("MY_INVESTORZ")
 st.markdown("# Data 🎈")
-df = pd.read_csv('./data/train.csv')
-df = df.set_index("PassengerId")
+df = pd.read_csv('./data/since_2022.csv')
+df = df.set_index("time")
 st.write("And here's the data for view:")
 sentence = st.text_input('Type if you want to research any information in the database:')
 # if sentence == df[0]:
