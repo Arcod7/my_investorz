0bdc893ed535d104a7e94acee1c906ef04f602fd
Matthias von Rakowski <matthias.von-rakowski@epitech.eu>
2023-02-12 13:37:36 +0100

    [FIX] Trainning bot

--- pages/Training.py
+++ pages/Training.py
@@ -6,13 +6,27 @@ import streamlit as st
 def Data():
     st.title("MY_INVESTORZ")
     st.markdown("# Thierry's training 🤖")
-    df = pd.read_csv('./data/since_2022.csv')
-    df = df.set_index("time")
-    st.write("And here's the data for view:")
-    sentence = st.text_input('Type if you want to research any information in the database:')
-    # if sentence == df[0]:
-        # st.write(my_model.predict(sentence))
-    # else:
+    df = pd.read_csv('./data/out.csv')
+    st.write("That's how Tireey is training:")
+    butb, butc = st.columns(2)
+    graph = []
+    with butb:
+        if st.button("Capital"):
+            graph.append("Capital")
+        elif "Capital" in graph:
+            graph.remoove("Capital")
+    with butc:
+        if st.button("ETH-USD"):
+            graph.append("ETH-USD")
+        elif "ETH-USD" in graph:
+            graph.remoove("ETH-USD")
+    if len(graph) == 0:
+        graph = ['Capital', 'ETH-USD']
+    st.line_chart(df[graph])
+    chart_data = pd.DataFrame(
+        df["Actions"],
+        columns = ["Actions"])
+    st.bar_chart(chart_data)
     st.write(df)
     st.sidebar.markdown("# Thierry's training 🤖")
 
